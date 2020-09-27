948d7d2e08c19a43ec47e9b0bf4f63cda3bfe828
amilanov75 <aleksmilanov@hotmail.com>
2020-09-27 16:01:09 +0100

    Add files via upload

---
+++ parcel/App.vue
@@ -0,0 +1,29 @@
+<template lang="html">
+  <div>
+    <navbar></navbar>
+    <!--<keep-alive>-->
+    <router-view></router-view>
+    <!-- </keep-alive> -->
+  </div>
+</template>
+
+<script>
+// import Vue from "vue";
+import navbar from "./NavBar.vue";
+
+
+export default {
+  name: "app",
+  components: {
+    navbar
+  },
+
+mounted() {
+  
+  uibuilder.start();
+
+}
+
+};
+</script>
+
